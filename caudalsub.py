4551ff312d585b2d5e02f83e7a13284b5992be60
cmirlop <carlosnds2000@gmail.com>
2024-12-11 19:14:13 +0100

    optimizacion

--- caudalsub.py
+++ caudalsub.py
@@ -67,15 +67,15 @@ def cargar_csv(rutaCSV):
 # ------Proceso ---#
 # Proceso de buscar en el diccionario, la fecha para escribir en las variables correspondientes
 # el valor de ese momento del excel
-def Proceso(file, var, dat_ant, node):
-    value_client = node
+def Proceso(file, var, val):
+    value_client = val
     # Buscamos el valor dentro del diccionario que hemos cargado anteriormente
     for x in range(len(file["Fecha"])):
         # Cuando la fecha coincide con el valor del cliente escribimos nuestros valores
         if file["Fecha"][x] == str(value_client.replace(tzinfo=None)):
             try:
                 var["Fecha"].write_value(datetime.strptime(file["Fecha"][x], "%Y-%m-%d %H:%M:%S").replace(tzinfo=None))  # Escribimos la hora obtenida del excel
-                dat_ant["FechaAnt"] = file["Fecha"][x]
+                
                 # Si el estado que leemos de esa hora es un fallo, escribimos:
                 # -1.0 en caudal
                 # "FALLO" en estado
@@ -91,53 +91,68 @@ def Proceso(file, var, dat_ant, node):
             except Exception as e:
                 print("Error: ", e)  # Si surge algun error, que nos lo imprima
 
-class DataChangeHandler:
-    def __init__(self, file, var, dat_ant,node2):
+#CONFIGURACION DE LA CLASE SUBCRIPTOR
+#Esta clase, se la asignamos a una variable para que cuando haya un cambio en una variable se llame a esta automaticamente
+class SubHandler:
+    def __init__(self, file, var,node2):
         self.file = file
         self.var = var
-        self.dat_ant = dat_ant
         self.node2 = node2
 
+    #Funcion que incorporamos a la clase para que cuando haya un cambio la llame, ya que es una funcion que cuando se recibe un cambio
+    # se llama automaticamente 
     def datachange_notification(self, node, val, attr):
         print(f"Cambio detectado en el cliente: {val}")
-        Proceso(self.file, self.var, self.dat_ant, val)
+        Proceso(self.file, self.var,  val) #Cuando tenemos un nuevo valor, llamamos a la funcion proceos que escribira los valores
 
 
 # ---- Lanzamiento del conjunto ------#
 def launch():
     # Variables
-    urlSer = "opc.tcp://0.0.0.0:4842/achu/embalse"
-    urlCli = "opc.tcp://0.0.0.0:4840/achu/horas"
-    uri = "http://www.achu.es/embalse"
-    rutXlm = "nodes_caudal.xml"
-    rutCSV = "poyo.csv"
-    id_type = "ns=1;i=2001"
-    id_obj = "2:Caudalimetro"
-    id_est = "2:Estado"
-    id_fec = "2:Fecha"
-    id_nodoCli = "ns=2;i=2"
+    urlSer = "opc.tcp://0.0.0.0:4842/achu/embalse" #URL del servidor
+    urlCli = "opc.tcp://0.0.0.0:4840/achu/horas" #URL del cliene
+    uri = "http://www.achu.es/embalse" #URI del servidor
+    rutXlm = "nodes_caudal.xml" #Si se encuentra en la misma ruta que el codigo se queda igual
+    rutCSV = "poyo.csv"#Ruta donde se encuentra el archivo CSV
+    id_type = "ns=1;i=2001" #Id del tipo de objeto "Caudalimetro"
+    id_obj = "2:Caudalimetro" #Nombre del objeto a buscar creado
+    id_est = "2:Estado" #Nombre de la propiedad Estado del objeto Caudalimetro
+    id_fec = "2:Fecha" #Nombre de la propiedad Fecha del objeto Caudalimetro
+    id_nodoCli = "ns=2;i=2" #Id del nodo de las horas que es el que queremos del cliente
 
+    #CONFIGURAMOS SERVER
+	#Creamos el servidor, objeto y arrancamos el server
     print("Configurando servidor")
     servidor, idx = crear_server(urlSer, rutXlm, uri)
     my_obj = crear_objeto(servidor, idx, id_type)
     servidor.start()
     print("Servidor arrancado correctamente")
 
-    var = buscar_objeto(my_obj, id_obj, id_est, id_fec)
+    #CONFIGURAMOS VARIABLES : Caudal, Estado, Fecha
+    var = buscar_objeto(my_obj, id_obj, id_est, id_fec) #Esta funcion nos devuelve un diccionario con las 3 variables
+
+    #CONFIGURACION DEL EXCEL
+	#Cargar excel en el codigo y lo transformamos a diccionario para acceder mas rapido
     file = cargar_csv(rutCSV)
+
+    
+	#CONFIGURAMOS CLIENTE
+	#Realizamos la conexion con el cliente a la URL que le pasamos, y obtenemos el nodo de las horas
+    print("Iniciando conexion con el cliente")
     cliente, node2 = configurar_cliente(urlCli, id_nodoCli)
+    print("Conexion realizada")
 
-    dat_ant = {"FechaAnt": None}
-
-    # Usamos una clase como manejador de cambios
-    handler = DataChangeHandler(file, var, dat_ant,node2)
-    sub = cliente.create_subscription(1000, handler)
-    sub.subscribe_data_change(node2)
+    #CONFIGURACION DEL SUBSCRIPTOR
+    handler = SubHandler(file,var,node2) #Creamos una variable del tipo SubHandel, que es la clase que contiene el metodo para poder realizar los cambios
+    sub = cliente.create_subscription(1000, handler) #Creamos la subscripcion con el cliente, asignandole el handler
+    sub.subscribe_data_change(node2) #Le decimos que se subscriba al nodo del cliente, que en este caso es el de las horas
 
     try:
         print("Servidor y cliente configurados. Esperando cambios...")
-        while True:
-            time.sleep(1)
+        while cliente:
+            time.sleep(1) #Mantenemos el servidor conectado
+    except ConnectionError as e:
+        print(f"Se perdio la conexion con el cliente: {e}")
     finally:
         print("Cerrando servidor")
         servidor.stop()
